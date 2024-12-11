6ce1334e84ea592188c8549a42c07e5c64880395
bliem69 <liembryan49@gmail.com>
2024-12-11 20:18:35 +0700

    first commit

---
+++ app/Http/Controllers/Backend/ProfileController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Faker\Core\File as CoreFile;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use File;
+
+class ProfileController extends Controller
+{
+    public function index(){
+        return view('admin.profile.index');
+    }
+
+    public function updateProfile(Request $request){
+        $request->validate([
+            'name' => ['required', 'max:100'],
+            'email' => ['required', 'email', 'unique:users,email,'.Auth::user()->id],
+            'image' => ['image', 'max:2048']
+        ]);
+
+        $user = Auth::user();
+
+        if($request->hasFile('image')){
+            if(File::exists(public_path($user->image))){
+                File::delete(public_path($user->image));
+            }
+            $image = $request->image;
+            $imageName = rand().'_'.$image->getClientOriginalName();
+            $image->move(public_path('upload'), $imageName);
+
+            $path = "/upload/".$imageName;
+
+            $user->image = $path;
+            
+        }
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->save();
+
+        toastr()->success('Profile Updated Succesfully!');
+        return redirect()->back();
+    }
+
+    public function updatePassword(Request $request){
+        $request->validate([
+            'current_password' => ['required', 'current_password'],
+            'password' => ['required', 'confirmed', 'min:8']
+        ]);
+
+        $request->user()->update([
+            'password' => bcrypt($request->password)
+        ]);
+
+
+        toastr()->success('Password Updated Succesfully!');
+        return redirect()->back();
+
+    }
+}
---
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use App\Models\Slider;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        $sliders = Slider::where('status', 1)->orderBy('serial', 'asc')->get();
+        return view('frontend.home.home', 
+            compact(
+                'sliders'
+            ));
+    }
+}
